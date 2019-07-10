e4a812b72881877cf4f1127f8da218180c3862a6
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-07-10 16:17:36 +0100

    Working

---
+++ default_variables.tf
@@ -0,0 +1,88 @@
+variable "project" {
+	default = "central-phalanx-246109"
+}
+
+variable "name" {
+	default = "test"
+}
+
+variable "zone" {
+	default = "europe-west2-c"
+}
+
+variable "machine_type" {
+	default = "n1-standard-1"
+}
+
+variable "image_project" {
+	default = "ubuntu-os-cloud"
+}
+
+variable "image" {
+	default = "centos-7"
+}
+
+variable "network" {
+	default = "default"
+}
+
+variable "ssh_user" {
+	default = "terraform"
+}
+
+variable "public_key" {
+	default = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key" {
+	default = "~/.ssh/id_rsa"
+}
+
+variable "package_manager" {
+	default = "yum"
+}
+
+variable "update_packages" {
+	default = {
+		"apt" = "sudo apt update && sudo apt upgrade -y"
+		"yum" = "sudo yum update -y"
+	}
+}
+
+variable "packages" {
+	default = [
+		"wget",
+		"unzip"
+	]
+}
+
+variable "install_packages" {
+	default = {
+		"yum" = "sudo yum install -y",
+		"apt" = "sudo apt install -y"
+	}
+}
+
+variable "scripts" {
+	default = []
+}
+
+variable "allowed_ports" {
+	default = ["22",
+		   "8080",
+		   "27017",
+		   "4200"
+		  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ variables/jenkins.tfvars
@@ -0,0 +1,6 @@
+project = "central-phalanx-246109"
+name = "jenkins"
+scripts = [
+        "scripts/test1"
+]
+~         
